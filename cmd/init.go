ede5121244da7cbb6f03f71734b04e9573aebbbf
si3nloong <sianloong90@gmail.com>
2023-03-30 10:49:16 +0800

    feat(init): polish and enhance the `init` command

--- cmd/init.go
+++ cmd/init.go
@@ -15,7 +15,17 @@ import (
 
 func initCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:  "init",
+		Use:   "init",
+		Short: "Set up a new or existing npm package.",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Println(`This utility will walk you through creating a sqlgen.yaml file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See ` + "`sqlgen init`" + ` for definitive documentation on these fields
+and exactly what they do.
+`)
+			return nil
+		},
 		RunE: runInitCommand,
 	}
 }
@@ -54,6 +64,22 @@ func runInitCommand(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
+	fileDest := filepath.Join(fileutil.Getpwd(), "sqlgen.yml")
+	cmd.Println("\nAbout to write to " + fileDest + ":\n")
+
+	var ok bool
+	if err := survey.AskOne(&survey.Confirm{
+		Message: "Is this OK?",
+		Default: true,
+	}, &ok); err != nil {
+		return err
+	}
+
+	// Do nothing when user choose "No".
+	if !ok {
+		return nil
+	}
+
 	w := bytes.NewBufferString("")
 	enc := yaml.NewEncoder(w)
 	enc.SetIndent(2)
@@ -62,7 +88,7 @@ func runInitCommand(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	if err := os.WriteFile(filepath.Join(fileutil.Getpwd(), "sqlgen.yml"), w.Bytes(), 0o644); err != nil {
+	if err := os.WriteFile(fileDest, w.Bytes(), 0o644); err != nil {
 		return err
 	}
 
