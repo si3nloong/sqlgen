6d25048f5ebe476d1f927b3c30f86254d8870f4c
si3nloong <sianloong90@gmail.com>
2023-11-12 15:31:50 +0800

    chore: rename `ColumnValuer`

--- sequel/column.go
+++ sequel/column.go
@@ -10,24 +10,24 @@ type ColumnValuer[T any] interface {
 	Value() driver.Value
 }
 
-type colValuer[T any] struct {
+type column[T any] struct {
 	colName string
 	convert ConvertFunc[T]
 	v       driver.Value
 }
 
-func (c colValuer[T]) ColumnName() string {
+func (c column[T]) ColumnName() string {
 	return c.colName
 }
 
-func (c colValuer[T]) Convert(v T) driver.Value {
+func (c column[T]) Convert(v T) driver.Value {
 	return c.convert(v)
 }
 
-func (c colValuer[T]) Value() driver.Value {
+func (c column[T]) Value() driver.Value {
 	return c.v
 }
 
 func Column[T any](columnName string, value T, convert ConvertFunc[T]) ColumnValuer[T] {
-	return colValuer[T]{colName: columnName, v: convert(value), convert: convert}
+	return column[T]{colName: columnName, v: convert(value), convert: convert}
 }
