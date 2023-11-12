236e842c23e8e47e445cdbd2c39222da32a9db3b
si3nloong <sianloong90@gmail.com>
2023-11-12 12:48:52 +0800

    test: add more tests

---
+++ sequel/dialect/postgres/postgres_test.go
@@ -0,0 +1,24 @@
+package postgres
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPostgresDriver(t *testing.T) {
+	driver := new(postgresDriver)
+	t.Run("Driver", func(t *testing.T) {
+		require.Equal(t, "postgres", driver.Driver())
+	})
+
+	t.Run("Var", func(t *testing.T) {
+		require.Equal(t, "$1", driver.Var(1))
+		require.Equal(t, "$10", driver.Var(10))
+	})
+
+	t.Run("Wrap", func(t *testing.T) {
+		require.Equal(t, `"abc"`, driver.Wrap("abc"))
+		require.Equal(t, `"abc_def"`, driver.Wrap("abc_def"))
+	})
+}
