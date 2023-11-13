dcb22a6215db97e3416bb434afac8a7fdfe8c77e
si3nloong <sianloong90@gmail.com>
2023-11-13 10:31:10 +0800

    fix: packages not loaded

--- codegen/interface.go
+++ codegen/interface.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	sqlValuer, sqlScanner,
-	sqlTabler, sqlColumner, sqlRower,
+	sqlTabler, sqlColumner,
 	binaryMarshaler, binaryUnmarshaler,
 	textMarshaler, textUnmarshaler *types.Interface
 )
@@ -16,18 +16,26 @@ var (
 func init() {
 	pkgs, err := packages.Load(&packages.Config{
 		Mode: packages.NeedTypes,
-	}, "database/sql...", "github.com/si3nloong/sqlgen/sequel", "encoding")
+	}, "database/sql...", "github.com/si3nloong/sqlgen...", "encoding")
 	if err != nil {
 		panic(err)
 	}
 
-	sqlValuer = pkgs[0].Types.Scope().Lookup("Valuer").Type().Underlying().(*types.Interface)
-	sqlScanner = pkgs[1].Types.Scope().Lookup("Scanner").Type().Underlying().(*types.Interface)
-	sqlTabler = pkgs[2].Types.Scope().Lookup("Tabler").Type().Underlying().(*types.Interface)
-	sqlColumner = pkgs[2].Types.Scope().Lookup("Columner").Type().Underlying().(*types.Interface)
-	sqlRower = pkgs[2].Types.Scope().Lookup("Valuer").Type().Underlying().(*types.Interface)
-	binaryMarshaler = pkgs[3].Types.Scope().Lookup("BinaryMarshaler").Type().Underlying().(*types.Interface)
-	binaryUnmarshaler = pkgs[3].Types.Scope().Lookup("BinaryUnmarshaler").Type().Underlying().(*types.Interface)
-	textMarshaler = pkgs[3].Types.Scope().Lookup("TextMarshaler").Type().Underlying().(*types.Interface)
-	textUnmarshaler = pkgs[3].Types.Scope().Lookup("TextUnmarshaler").Type().Underlying().(*types.Interface)
+	for _, p := range pkgs {
+		switch p.ID {
+		case "database/sql/driver":
+			sqlValuer = p.Types.Scope().Lookup("Valuer").Type().Underlying().(*types.Interface)
+		case "database/sql":
+			sqlScanner = p.Types.Scope().Lookup("Scanner").Type().Underlying().(*types.Interface)
+		case "encoding":
+			binaryMarshaler = p.Types.Scope().Lookup("BinaryMarshaler").Type().Underlying().(*types.Interface)
+			binaryUnmarshaler = p.Types.Scope().Lookup("BinaryUnmarshaler").Type().Underlying().(*types.Interface)
+			textMarshaler = p.Types.Scope().Lookup("TextMarshaler").Type().Underlying().(*types.Interface)
+			textUnmarshaler = p.Types.Scope().Lookup("TextUnmarshaler").Type().Underlying().(*types.Interface)
+		case "github.com/si3nloong/sqlgen/sequel":
+			sqlTabler = p.Types.Scope().Lookup("Tabler").Type().Underlying().(*types.Interface)
+			sqlColumner = p.Types.Scope().Lookup("Columner").Type().Underlying().(*types.Interface)
+			// sqlRower = p.Types.Scope().Lookup("Valuer").Type().Underlying().(*types.Interface)
+		}
+	}
 }
