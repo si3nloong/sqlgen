d756b1d2920bb7b73f2a94c78c1eecbb277760b6
si3nloong <sianloong90@gmail.com>
2024-06-25 14:15:11 +0800

    fix: schema should have default value

--- sequel/dialect/postgres/data_type.go
+++ sequel/dialect/postgres/data_type.go
@@ -1,9 +1,12 @@
 package postgres
 
 import (
+	"database/sql"
 	"fmt"
 	"go/types"
+	"strconv"
 	"strings"
+	"unsafe"
 
 	"github.com/si3nloong/sqlgen/codegen/templates"
 )
@@ -29,59 +32,59 @@ func (d *postgresDriver) dataType(f *templates.Field) (dataType string) {
 
 		switch t.String() {
 		case "rune":
-			return "CHAR(1)" + notNull(len(ptrs) > 0)
+			return "CHAR(1)" + notNullDefault(ptrs)
 		case "int8", "int16":
-			return "INT2" + notNull(len(ptrs) > 0)
+			return "INT2" + notNullDefault(ptrs, 0)
 		case "int32", "int":
-			return "INT" + notNull(len(ptrs) > 0)
+			return "INT" + notNullDefault(ptrs, 0)
 		case "int64":
-			return "INT8" + notNull(len(ptrs) > 0)
+			return "INT8" + notNullDefault(ptrs, 0)
 		case "bool":
-			return "BOOL" + notNull(len(ptrs) > 0)
+			return "BOOL" + notNullDefault(ptrs, false)
 		case "uint8", "uint16", "byte":
-			return "INT2" + notNull(len(ptrs) > 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
+			return "INT2" + notNullDefault(ptrs, 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
 		case "uint32", "uint":
-			return "INT" + notNull(len(ptrs) > 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
+			return "INT" + notNullDefault(ptrs, 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
 		case "uint64":
-			return "INT8" + notNull(len(ptrs) > 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
+			return "INT8" + notNullDefault(ptrs, 0) + " CHECK(" + d.QuoteIdentifier(f.ColumnName) + " >= 0)"
 		case "float32":
-			return "DOUBLE PRECISION" + notNull(len(ptrs) > 0)
+			return "DOUBLE PRECISION" + notNullDefault(ptrs, 0.0)
 		case "float64":
-			return "DOUBLE PRECISION" + notNull(len(ptrs) > 0)
+			return "DOUBLE PRECISION" + notNullDefault(ptrs, 0.0)
 		case "cloud.google.com/go/civil.Date":
-			return "DATE" + notNull(len(ptrs) > 0)
+			return "DATE" + notNullDefault(ptrs, sql.RawBytes(`CURRENT_DATE`))
 		case "time.Time":
 			var size int
 			if f.Size > 0 && f.Size < 7 {
 				size = f.Size
 			}
 			if size > 0 {
-				return fmt.Sprintf("TIMESTAMP(%d)", size) + notNull(len(ptrs) > 0)
+				return fmt.Sprintf("TIMESTAMP(%d)", size) + notNullDefault(ptrs, sql.RawBytes(`NOW()`))
 			}
-			return "TIMESTAMP" + notNull(len(ptrs) > 0)
+			return "TIMESTAMP" + notNullDefault(ptrs, sql.RawBytes(`NOW()`))
 		case "string":
 			size := 255
 			if f.Size > 0 {
 				size = f.Size
 			}
-			return fmt.Sprintf("VARCHAR(%d)", size) + notNull(len(ptrs) > 0)
+			return fmt.Sprintf("VARCHAR(%d)", size) + notNullDefault(ptrs, "")
 		case "[]rune":
-			return "VARCHAR(255)" + notNull(len(ptrs) > 0)
+			return "VARCHAR(255)" + notNullDefault(ptrs)
 		case "[]byte":
-			return "BYTEA" + notNull(len(ptrs) > 0)
+			return "BYTEA" + notNullDefault(ptrs)
 		case "[16]byte":
 			if f.IsBinary {
 				return "BIT(16)"
 			}
 			return "VARBIT(36)"
 		case "encoding/json.RawMessage":
-			return "VARBIT" + notNull(len(ptrs) > 0)
+			return "VARBIT" + notNullDefault(ptrs)
 		default:
 			if strings.HasPrefix(t.String(), "[]") {
 				if f.IsBinary {
-					return "JSONB" + notNull(len(ptrs) > 0)
+					return "JSONB" + notNullDefault(ptrs)
 				}
-				return "JSON" + notNull(len(ptrs) > 0)
+				return "JSON" + notNullDefault(ptrs)
 			}
 		}
 		if prev == t {
@@ -89,12 +92,34 @@ func (d *postgresDriver) dataType(f *templates.Field) (dataType string) {
 		}
 		t = prev
 	}
-	return "VARCHAR(255)" + notNull(len(ptrs) > 0)
+	return "VARCHAR(255)" + notNullDefault(ptrs)
 }
 
-func notNull(isNull bool) string {
-	if isNull {
+func notNullDefault(ptrs []types.Type, defaultValue ...any) string {
+	if len(ptrs) > 0 {
 		return ""
 	}
+	if len(defaultValue) > 0 {
+		return " NOT NULL DEFAULT " + format(defaultValue[0])
+	}
 	return " NOT NULL"
+}
+
+func format(v any) string {
+	switch vi := v.(type) {
+	case string:
+		return "'" + vi + "'"
+	case bool:
+		return strconv.FormatBool(vi)
+	case int:
+		return strconv.Itoa(vi)
+	case float32:
+		return strconv.FormatFloat(float64(vi), 'f', -1, 64)
+	case float64:
+		return strconv.FormatFloat(vi, 'f', -1, 64)
+	case sql.RawBytes:
+		return unsafe.String(unsafe.SliceData(vi), len(vi))
+	default:
+		panic("unsupported data type")
+	}
 }
